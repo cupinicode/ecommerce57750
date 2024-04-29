8a5b5cfdb1cab03e8f1f91c1b31c5c78e1b438d4
Jorge Cupini <jorge.cupini@bue.edu.ar>
2024-04-29 01:12:12 -0300

    Commit 3

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,10 @@
-import cartImage from './assets/shopping-cart-outline.svg'
-import classes from './CartWidget.module.css'
+import { CgShoppingCart } from "react-icons/cg"; /* Es un componente de React */
+/* import classes from './CartWidget.module.css' */
 
 const CartWidget = () => {
     return (
         <div>
-            <img src={ cartImage } className={ classes.CartIcon }   alt="Carrito de Compras" />
+            <CgShoppingCart color="red" size={30}/> {/* color y size son los parametros que le paso al componente */}
             0
         </div>
     )
